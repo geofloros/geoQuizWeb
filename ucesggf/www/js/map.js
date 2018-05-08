a25202274fcd48c86c200aa6d88635681f606e3b
geofloros <flwrosg@gmail.com>
2018-05-08 14:52:49 -0400

    refs update

--- ucesggf/www/js/map.js
+++ ucesggf/www/js/map.js
@@ -1,3 +1,4 @@
+//Source: Adapted from Ellul, 2018: Web and Mobile Apps and Programming – Leaflet and Javascript Part 1
 // load the map
 		var mymap2 = L.map('mapid').setView([51.524559, -0.134040], 14);
 // load the tiles
